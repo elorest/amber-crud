1b1764ad754948978d0ba7c51c7062b0433b6cba
Isaac Sloan <isaac@isaacsloan.com>
2017-05-22 22:38:16 -0600

    moved routes to it's own file

--- src/demo.cr
+++ src/demo.cr
@@ -8,6 +8,7 @@
 require "amber"
 require "./**"
 require "./*"
+require "../config/*"
 
 MY_APP_SERVER = Amber::Server.instance
 
@@ -27,32 +28,6 @@ MY_APP_SERVER.config do |app|
   # of which transformation to run for each of the app requests.
 
   # All api scoped routes will run these transformations
-  pipeline :web do
-    # Plug is the method to use connect a pipe (middleware)
-    # A plug accepts an instance of HTTP::Handler
-    # plug Amber::Pipe::Params.new
-    plug Amber::Pipe::Logger.new
-    plug Amber::Pipe::Flash.new
-    plug Amber::Pipe::Session.new
-    # plug Amber::Pipe::CSRF.new
-  end
-
-  # All static content will run these transformations
-  pipeline :static do
-    plug HTTP::StaticFileHandler.new("./public")
-    plug HTTP::CompressHandler.new
-  end
-
-  routes :static do
-    # Each route is defined as follow
-    # verb resource : String, controller : Symbol, action : Symbol
-    get "/*", StaticController, :index
-  end
-
-  routes :web do
-    resources "/posts", PostController
-    get "/", HomeController, :index
-  end
   # This is how you define the routes for your application
   # HTTP methods supported [GET, PATCH, POST, PUT, DELETE, OPTIONS]
   # Read more about HTTP methods here
